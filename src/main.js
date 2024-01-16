21a85446b1ce83cd7592944685eabed070ba92fd
Tarimari <tarashorban1987@gmail.com>
2024-01-16 11:38:30 +0200

    1601

--- src/main.js
+++ src/main.js
@@ -1,10 +1,59 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-// import { createLogger } from "vite";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector(`.submit-form`);
+const list = document.querySelector(`.gallery`);
 form.addEventListener(`submit`, searchRequest);
-function searchRequest(characteristics) {
+
+
+function searchRequest(evt) {
+    // evt.preventDefault();
+    const characteristics = evt.currentTarget.elements.search.value;
+    getRequest(characteristics)
+        .then(data => (list.innerHTML = createMarkup(data.hits)))
+        .catch(err => console.log(err))
+
+}
+
+function createMarkup(arr) {
+    if (arr.total === 0) {
+    iziToast.show({
+    message: '❌ Sorry, there are no images matching your search query. Please try again!'
+    });
+    }
+    else {
+        return arr.map( ({webformatURL, largeImageURL, tags, likes, views, comments, downloads} ) => `
+        <li class="gallery-item">
+          <img
+            srcset='${webformatURL} 1x, ${largeImageURL} 2x'
+            src='${webformatURL}'
+            alt='${tags}'
+          />
+        </li>
+         <div class="gallery-stats">
+            <li class="gallery-stats-item">
+              <h3 class="stats-head">Likes</h3>
+              <p class="stats-indicator">${likes}</p>
+            </li>
+            <li class="gallery-stats-item">
+              <h3 class="stats-head">Views</h3>
+              <p class="stats-indicator">${views}</p>
+            </li>
+            <li class="gallery-stats-item">
+              <h3 class="stats-head">Comments</h3>
+              <p class="stats-indicator">${comments}</p>
+            </li>
+            <li class="gallery-stats-item">
+              <h3 class="stats-head">Downloads</h3>
+              <p class="stats-indicator">${downloads}</p>
+            </li>
+          </div>`).join(``)
+    }
+}
+
+function getRequest(characteristics) {
 
     const BASE_URL = `https://pixabay.com/api/`;
     const API_KEY = `41768952-3eb5a1819d194e4ebd739434d`
@@ -19,9 +68,5 @@ function searchRequest(characteristics) {
 
 }
 
-searchRequest(`cat`).then(data => console.log(data)).catch(err => console.log(err))
 
-iziToast.show({
-    message: '❌ Sorry, there are no images matching your search query. Please try again!'
-});
 
